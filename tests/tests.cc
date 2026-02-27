dcb4b3aec07ba07d3a9affcdad799ed37c9086d3
anshs527 <anshshah527@gmail.com>
2026-02-27 20:36:13 +0000

    tested deposit

--- tests/tests.cc
+++ tests/tests.cc
@@ -51,10 +51,10 @@ TEST_CASE("Example: Create a new account", "[ex-1]") {
   REQUIRE(transactions[{12345678, 1234}] == empty);
 }
 
-TEST_CASE("Example: Simple widthdraw", "[ex-2]") {
+TEST_CASE("Example: Simple withdraw", "[ex-2]") {
   Atm atm;
   atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
-  atm.WithdrawCash(12345678, 1234, 20);
+  atm.WithdrawCash(12345678, 1234, 20.0);
   auto accounts = atm.GetAccounts();
   Account sam_account = accounts[{12345678, 1234}];
 
@@ -74,12 +74,15 @@ TEST_CASE("Example: Print Prompt Ledger", "[ex-3]") {
   atm.PrintLedger("./prompt.txt", 12345678, 1234);
   REQUIRE(CompareFiles("./ex-1.txt", "./prompt.txt"));
 }
-TEST_CASE("Example: Simple widthdraw", "[ex-2]") {
+TEST_CASE("MyCase 1", "[ex-4]") {
   Atm atm;
-  atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30);
-  atm.WithdrawCash(12345678, 1234, 20);
+  atm.RegisterAccount(99999, 1234, "Sam Sepiol", 299.99);
+  atm.WithdrawCash(99999, 1234, 20.0);
   auto accounts = atm.GetAccounts();
-  Account sam_account = accounts[{12345678, 1234}];
-
-  REQUIRE(sam_account.balance == 280.30);
+  REQUIRE(accounts.size() == 1);
+  Account sam_account = accounts[{99999, 1234}];
+  REQUIRE(sam_account.balance == 279.99);
+  atm.DepositCash(99999, 1234, 100.0);
+  sam_account = accounts[{99999, 1234}];
+  REQUIRE(sam_account.balance == 379.99);
 }
